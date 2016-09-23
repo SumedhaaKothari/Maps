87e39d89256b333b8ef55a7162b8723fb851dd21
Zachary Cotton <mr.zacharycotton@gmail.com>
2016-09-22 21:43:31 -0700

    closest restaurant

--- recommend.py
+++ recommend.py
@@ -19,7 +19,7 @@ def find_closest(location, centroids):
     [2.0, 3.0]
     """
     # BEGIN Question 3
-    "*** REPLACE THIS LINE ***"
+    return min(centroids, key=lambda x: distance(location,x))
     # END Question 3
 
 
